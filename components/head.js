48a37b245a63c440632d7938c7a32dba7476be6f
idadhamz <dadiilhamsetiadii@gmail.com>
2021-09-05 22:22:07 +0700

    update: meta head

--- components/head.js
+++ components/head.js
@@ -6,12 +6,13 @@ export default function index({ data }) {
             <meta charset="UTF-8"></meta>
             <title>{data.title}</title>
             <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-            <meta name="description" content={data.description}></meta>
+            <meta name="description" content={data.description} />
+            <meta name="keywords" content="dadi, idad, idadilham" />
             <meta name="google-site-verification" content="QtPLOMbLk5ajIwIdcq0yrxGvpF5Djm9hrsVk7kMl-6g" />
 
             <meta property="og:title" content={data.title} key="ogtitle" />
             <meta property="og:description" content={data.description} key="ogdesc" />
-            <meta property="og:image" content="assets/img/fotoVectorMeta.jpg" key="ogimage" />
+            <meta property="og:image" content="/assets/img/fotoVectorMeta.jpg" key="ogimage" />
         </Head>
     )
 }
