82724d8152c2a484de36a433df1803ed85770da6
idadilham <dadiilhamsetiadii@gmail.com>
2022-03-03 22:31:37 +0700

    update work history ui

--- components/WorkHistory/index.js
+++ components/WorkHistory/index.js
@@ -9,11 +9,11 @@ export default function WorkHistory() {
         </span>{" "}
         History
       </h1>
-      <div className="container grid grid-cols-1 sm:grid-cols-3 mx-auto">
+      <div className="container grid grid-cols-1 sm:grid-cols-3 gap-2 sm:gap-4 mx-auto">
         {listWork
           .sort((a, b) => b.id - a.id)
           .map((data, index) => (
-            <div className="col-span-1 gap-2 sm:gap-4" key={index}>
+            <div className="col-span-1" key={index}>
               <div className="sm:space-y-4 py-2 sm:py-4">
                 <h1 className="mb-1 text-lg font-semibold leading-relaxed sm:text-xl title-font">
                   {data.job}
